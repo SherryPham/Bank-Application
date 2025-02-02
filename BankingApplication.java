508cbbd90c6b29709bd84a7209b514f040470f98
SherryPham <phamthuthu204@gmail.com>
2025-02-02 21:49:47 +1100

    deposit method

---
+++ BankingApplication.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BankingApplication {
+    public static void main(String[] args) {
+       
+    }
+}
+
+
+class BankAccount
+{
+    int balance;
+    int previousTransaction;
+    String customerName;
+    String customerId;
+
+    void deposit(int amount)
+    {
+        if(amount != 0)
+        {
+            balance = balance + amount;
+            previousTransaction = amount;
+        }
+    }
+}
